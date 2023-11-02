d976645c827dcf745c5af9b6d6884eec9e71300c
fdeboom <faithdeboom@gmail.com>
2023-11-02 11:00:42 -0400

    fixed screen display function to contain tuple

--- main.py
+++ main.py
@@ -3,10 +3,12 @@ import pygame
 
 pygame.init()
 
-screen = pygame.display.set_mode(800, 600)
+screen = pygame.display.set_mode((800, 600))
 pygame.display.set_caption("The best game ever")  # game name
 
 running = True
 while running:
     for event in pygame.event.get():
         pass
+
+    pygame.display.flip()  #updates your changes

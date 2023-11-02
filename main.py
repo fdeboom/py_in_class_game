248f66560450658226d686816988052f9d497475
fdeboom <faithdeboom@gmail.com>
2023-11-02 11:09:05 -0400

    blue rectangle added

--- main.py
+++ main.py
@@ -3,12 +3,12 @@ import pygame
 
 pygame.init()
 
-screen = pygame.display.set_mode((800, 600))
+screen = pygame.display.set_mode((1200, 800))
 pygame.display.set_caption("The best game ever")  # game name
 
 running = True
 while running:
     for event in pygame.event.get():
-        pass
+        pygame.draw.rect(screen, (0, 0, 255), (600, 400, 300, 200))
 
-    pygame.display.flip()  #updates your changes
+    pygame.display.flip()  # updates your changes at the end of loop
